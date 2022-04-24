cd05605884cae69be95d42bede16d823626cdb93
Guilherme Nunes <guilhermemmnunes@gmail.com>
2022-04-24 17:31:21 +0100

    t2 e t4 merged, format

--- Base_Project2Share/src/OctreeOps.scala
+++ Base_Project2Share/src/OctreeOps.scala
@@ -7,13 +7,13 @@ object OctreeOps {
 
   def generateOcTree(root: Placement, list: List[Node], maxDepth: Int): Octree[Placement] = {
     println(s"root: ${root}")
-    if(maxDepth == 0 || !SpaceOps.areModelsWithin(list, SpaceOps.createBox(root))) OcEmpty
+    if (maxDepth == 0 || !SpaceOps.areModelsWithin(list, SpaceOps.createBox(root))) OcEmpty
     else {
       val appropriateModels = SpaceOps.filterAppropriateModelsForPlacement(list, root)
       appropriateModels match {
-        case x::y => {
-          SpaceOps.printModels(x::y);
-          OcLeaf(root, x::y)
+        case x :: y => {
+          SpaceOps.printModels(x :: y);
+          OcLeaf(root, x :: y)
         }
         case List() => OcNode[Placement](
           root,
@@ -37,24 +37,25 @@ object OctreeOps {
     generateOcTree(root, models, maxDepth)
   }
 
-  def scaleOctree(fact:Double, oct:Octree[Placement]):Octree[Placement] = {
-    def scale3DModels(fact:Double, lst:List[Node]):List[Node]= {
+  def scaleOctree(fact: Double, oct: Octree[Placement]): Octree[Placement] = {
+    def scale3DModels(fact: Double, lst: List[Node]): List[Node] = {
       lst match {
         case List() => List()
-        case x::xs =>
+        case x :: xs =>
           x.setScaleX(fact)
           x.setScaleY(fact)
           x.setScaleZ(fact)
-          x::scale3DModels(fact,xs)
+          x :: scale3DModels(fact, xs)
       }
     }
+
     oct match {
       case OcEmpty => OcEmpty
-      case OcLeaf(section: Section) => OcLeaf(section._1._1,section._1._2*fact,scale3DModels(fact,section._2))
-      case OcNode(((x,y,z),size),oc1,oc2,oc3,oc4,oc5,oc6,oc7,oc8) =>
-        OcNode(((x,y,z),size*fact),scaleOctree(fact,oc1),scaleOctree(fact,oc2),
-          scaleOctree(fact,oc3),scaleOctree(fact,oc4),scaleOctree(fact,oc5),scaleOctree(fact,oc6),
-          scaleOctree(fact,oc7),scaleOctree(fact,oc8))
+      case OcLeaf(section: Section) => OcLeaf(section._1._1, section._1._2 * fact, scale3DModels(fact, section._2))
+      case OcNode(((x, y, z), size), oc1, oc2, oc3, oc4, oc5, oc6, oc7, oc8) =>
+        OcNode(((x, y, z), size * fact), scaleOctree(fact, oc1), scaleOctree(fact, oc2),
+          scaleOctree(fact, oc3), scaleOctree(fact, oc4), scaleOctree(fact, oc5), scaleOctree(fact, oc6),
+          scaleOctree(fact, oc7), scaleOctree(fact, oc8))
     }
   }
 
